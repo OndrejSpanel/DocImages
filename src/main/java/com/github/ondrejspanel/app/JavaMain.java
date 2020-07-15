24b3b7b238841aa178d55388394abe041a9c50b0
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-07-15 15:41:49 +0200

    Demonstrate JavaDoc / ScalaDoc images

---
+++ src/main/java/com/github/ondrejspanel/app/JavaMain.java
@@ -0,0 +1,13 @@
+package com.github.ondrejspanel.app;
+
+/** JavaMain - application entry point (written in Java)
+ * <br/>
+ * <img src="main.png"/>
+ *
+ */
+
+public class JavaMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/scala/com/github/ondrejspanel/doc/Main.scala
@@ -0,0 +1,11 @@
+package com.github.ondrejspanel.doc
+
+/** Main - application entry point
+ * <br/>
+ * <img src="main.png"/>
+ *
+ */
+
+object Main extends App {
+
+}
